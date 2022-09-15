ae4b13c3332423e347e1e4ec6fc80fbf7f98b40c
ndavid3 <ndavid21@mailbox.unideb.hu>
2022-09-15 21:29:58 +0200

    first homework upload

---
+++ Code/first hw/first.py
@@ -0,0 +1,21 @@
+def main():
+    txt = input("Adjon meg egy mondatot: \n")
+    wl = {}
+
+    for k in txt:
+        if k in wl:
+            wl[k] += 1
+        else:
+            wl[k] = 1
+
+    print("Betűk gyakorisága:", wl)
+
+    print("Fordítva: ", txt[::-1])
+
+    rev = {}
+    rev = txt.split()
+
+    print("Listába rendezve szavanként: ", rev)
+
+
+main()
---
+++ Code/first hw/second.py
@@ -0,0 +1,12 @@
+def main():
+    ln = int(input("Adjon meg egy számot és egy mértékegységet (cm/inch): \n"))
+    tp = input()
+
+    if tp == "cm":
+        print(ln/2.54, "inches")
+    elif tp == "inch":
+        print(ln*2.54, "cm")
+    else:
+        print("Not correct unit!")
+
+main()
