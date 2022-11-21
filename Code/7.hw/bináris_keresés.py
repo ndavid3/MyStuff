2b244545f233a5d3affe423b93942c6b223a0a5d
ndavid3 <ndavid21@mailbox.unideb.hu>
2022-11-21 17:55:05 +0100

    homework upload

--- Code/7.hw/bináris_keresés.py
+++ Code/7.hw/bináris_keresés.py
@@ -2,6 +2,7 @@ def binkeres():
     A = [2, 5, 6, 8, 13, 32, 42, 50, 53, 62, 66, 70, 80, 89, 91]
     szm = 70
 
+    lep = 0
     n = len(A)
     also = 0
     felso = n-1
@@ -9,17 +10,24 @@ def binkeres():
     while(also <= felso):
         kozep = (also + felso) // 2
         if szm < A[kozep]:
+            lep += 1
             felso = kozep - 1
         elif szm > A[kozep]:
+            lep += 1
             also = kozep + 1
         else:
+            lep += 1
             ki = True, szm
             break
 
     else:
         ki = False
+
+    if ki != False:
+        print(str(ki) +", a " + str(lep) + " lépésben találta meg" + ", alsó: " + str(also) + ", felsö: " + str(felso) +", a k értéke: " + str(kozep))
+    else:
+        print("Nem találta meg")
     
-    return ki
 
 if __name__ == '__main__':
-    print(binkeres())
+    binkeres()
